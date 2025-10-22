a45939116dab681b9a64e3665aad985fe0b4d9a9
Leonardo Silva e Cruz <101243664+HiperD@users.noreply.github.com>
2025-10-21 23:45:35 -0300

    Codigos existentes

---
+++ config/db_sequelize.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('web_back2', 'postgres', '42456876', {
+    host: 'localhost',
+    dialect: 'postgres'
+  });
+
+var db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.Usuario = require('../models/relational/usuario.js')(sequelize, Sequelize);
+db.Receita = require('../models/relational/receita.js')(sequelize, Sequelize);
+db.Categoria = require('../models/relational/categoria.js')(sequelize, Sequelize);
+db.Categoria.hasMany(db.Receita, {foreignKey:'categoriaId', onDelete: 'NO ACTION'});
+module.exports = db;
+
